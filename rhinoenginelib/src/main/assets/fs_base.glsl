1348d262f274ca5df2d0e7af15fe2ec97eb0b2fa
MarcoCiaramella <marcociaramella87@gmail.com>
2021-01-02 17:54:53 +0100

    fixed bug

--- rhinoenginelib/src/main/assets/fs_base.glsl
+++ rhinoenginelib/src/main/assets/fs_base.glsl
@@ -4,7 +4,7 @@ precision highp float;
 
 
 uniform vec3 uLightsPos[1];
-uniform vec4 uLightsColor[1];
+uniform vec3 uLightsColor[1];
 uniform float uLightsIntensity[1];
 varying vec4 vColor;
 varying vec3 vPosition;
@@ -18,7 +18,7 @@ float calcDiffuseLight(vec3 light, float intensity, vec3 vertex, vec3 normal);
 
 void main() {
     float diffuse = calcDiffuseLight(uLightsPos[0], uLightsIntensity[0], vPosition, vNormal);
-    vec4 color = vColor * (diffuse*uLightsColor[0]);
+    vec4 color = vColor * (diffuse*vec4(uLightsColor[0],1.0));
     gl_FragColor = color;
 }
 
