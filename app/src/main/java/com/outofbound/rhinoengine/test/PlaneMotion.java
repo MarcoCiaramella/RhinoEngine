1a323ca489f5a88c6a45b04bbcc804ff568f2e5b
MarcoCiaramella <marcociaramella87@gmail.com>
2021-01-14 17:33:27 +0100

    new lighting

--- app/src/main/java/com/outofbound/rhinoengine/test/PlaneMotion.java
+++ app/src/main/java/com/outofbound/rhinoengine/test/PlaneMotion.java
@@ -6,9 +6,7 @@ import com.outofbound.rhinoenginelib.util.number.Numbers;
 public class PlaneMotion extends Motion {
 
     public PlaneMotion(){
-        scale.x = 10f;
-        scale.y = 0.1f;
-        scale.z = 10f;
+        scale = 10f;
     }
 
     @Override
