c16d82866e1dc38ee20dc87e7d2fae2cde173064
MarcoCiaramella <marcociaramella87@gmail.com>
2021-01-17 01:36:54 +0100

    new lighting

--- rhinoenginelib/src/main/assets/fs_scene.glsl
+++ rhinoenginelib/src/main/assets/fs_scene.glsl
@@ -2,26 +2,22 @@
 
 precision highp float;
 
-struct light_dir {
+
+struct DirLight {
     vec3 direction;
-    vec3 color;
-    float ambientStrength;
-    float specularStrength;
+    vec3 ambientColor;
+    vec3 diffuseColor;
+    vec3 specularColor;
 };
 
-uniform light_dir light;
+uniform DirLight dirLight;
 uniform vec3 uViewPos;
 varying vec4 vColor;
 varying vec3 vPosition;
 varying vec3 vNormal;
-const float specularStrength = 0.5;
-const float ambientStrength = 0.1;
 
 
-
-vec3 ambientLighting(vec3 lightColor);
-vec3 diffuseLighting(vec3 norm, vec3 lightDir, vec3 lightColor);
-vec3 specularLighting(vec3 norm, vec3 lightDir, vec3 lightColor);
+vec4 calcDirLight(vec3 normal);
 
 
 
@@ -34,19 +30,24 @@ void main() {
     gl_FragColor = vColor * vec4(ambient + diffuse + specular, 1.0);
 }
 
-vec3 ambientLighting(vec3 lightColor){
-    return ambientStrength * lightColor;
+float diffuseLighting(vec3 norm, vec3 lightDir){
+    return max(dot(norm, lightDir), 0.0);
 }
 
-vec3 diffuseLighting(vec3 norm, vec3 lightDir, vec3 lightColor){
-    return max(dot(norm, lightDir), 0.0) * lightColor;
-}
-
-vec3 specularLighting(vec3 norm, vec3 lightDir, vec3 lightColor){
+float specularLighting(vec3 norm, vec3 lightDir){
     vec3 viewDir = normalize(uViewPos - vPosition);
     vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(uViewPos, reflectDir), 0.0), 32.0);
-    return specularStrength * spec * lightColor;
+    return pow(max(dot(uViewPos, reflectDir), 0.0), 32.0);
+}
+
+vec4 calcDirLight(vec3 normal){
+    vec3 lightDir = normalize(-dirLight.direction);
+    float diff = diffuseLighting(normal,lightDir);
+    float spec = specularLighting(normal,lightDir);
+    vec4 ambient = vec4(dirLight.ambientColor,1.0) * vColor;
+    vec4 diffuse = vec4(dirLight.diffuseColor * diff, 1.0) * vColor;
+    vec4 specular = vec4(dirLight.specularColor * spec, 1.0) * vec4(0.5,0.5,0.5,1.0);
+    return ambient + diffuse + specualr;
 }
 
 
