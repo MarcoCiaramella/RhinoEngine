bd98046defae7d0b999ecaf16343144a4c9d9c91
MarcoCiaramella <marcociaramella87@gmail.com>
2021-01-21 00:05:08 +0100

    fixed error

--- rhinoenginelib/src/main/assets/fs_scene.glsl
+++ rhinoenginelib/src/main/assets/fs_scene.glsl
@@ -33,7 +33,7 @@ uniform PointLight uPointLights[MAX_NUM_POINT_LIGHTS];
 uniform int uNumPointLights;
 uniform vec3 uViewPos;
 varying vec4 vColor;
-varying vec3 vPosition;
+varying vec4 vPosition;
 varying vec3 vNormal;
 const vec4 bitShifts = vec4(1.0 / (256.0*256.0*256.0), 1.0 / (256.0*256.0), 1.0 / 256.0, 1.0);
 
@@ -73,7 +73,7 @@ vec4 calcDirLight(vec3 normal, vec3 viewDir){
     vec4 ambient = vec4(uDirLight.ambientColor, 1.0) * vColor;
     vec4 diffuse = vec4(uDirLight.diffuseColor * diff, 1.0) * vColor;
     vec4 specular = vec4(uDirLight.specularColor * spec, 1.0) * vec4(0.5,0.5,0.5,1.0);
-    return ambient + (diffuse + specular) * calcShadow(vec4(vPosition, 1.0) * uDirLight.shadowMVPMatrix, uDirLight.shadowMap, uDirLight.shadowEnabled);
+    return ambient + (diffuse + specular) * calcShadow(vPosition * uDirLight.shadowMVPMatrix, uDirLight.shadowMap, uDirLight.shadowEnabled);
 }
 
 float calcAttenuation(PointLight pointLight, float distance){
@@ -81,7 +81,7 @@ float calcAttenuation(PointLight pointLight, float distance){
 }
 
 vec4 calcPointLight(PointLight pointLight, vec3 normal, vec3 viewDir){
-    vec3 d = pointLight.position - vPosition;
+    vec3 d = pointLight.position - vec3(vPosition);
     vec3 lightDir = normalize(d);
     float diff = diffuseLighting(normal, lightDir);
     float spec = specularLighting(normal, lightDir, viewDir);
@@ -93,14 +93,14 @@ vec4 calcPointLight(PointLight pointLight, vec3 normal, vec3 viewDir){
     ambient *= attenuation;
     diffuse *= attenuation;
     specular *= attenuation;
-    return ambient + (diffuse + specular) * calcShadow(vec4(vPosition, 1.0) * pointLight.shadowMVPMatrix, pointLight.shadowMap, pointLight.shadowEnabled);
+    return ambient + (diffuse + specular) * calcShadow(vPosition * pointLight.shadowMVPMatrix, pointLight.shadowMap, pointLight.shadowEnabled);
 }
 
 
 
 void main() {
     vec3 normal = normalize(vNormal);
-    vec3 viewDir = normalize(uViewPos - vPosition);
+    vec3 viewDir = normalize(uViewPos - vec3(vPosition));
     vec4 result = calcDirLight(normal, viewDir);
     for (int i = 0; i < uNumPointLights; i++){
         result += calcPointLight(uPointLights[i], normal, viewDir);
--- rhinoenginelib/src/main/assets/vs_scene.glsl
+++ rhinoenginelib/src/main/assets/vs_scene.glsl
@@ -9,13 +9,13 @@ attribute vec4 aPosition;
 attribute vec3 aNormal;
 attribute vec4 aColor;
 varying vec4 vColor;
-varying vec3 vPosition;
+varying vec4 vPosition;
 varying vec3 vNormal;
 
 
 
 void main() {
-    vPosition = vec3(uMMatrix * aPosition);
+    vPosition = uMMatrix * aPosition;
     vNormal = normalize(vec3(uMMatrix * vec4(aNormal, 0.0)));
     vColor = aColor;
     gl_Position = uMVPMatrix * aPosition;
