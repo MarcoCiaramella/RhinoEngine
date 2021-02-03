f593ffda9b9fca3450f1958a9fa89e6a52fde3c9
MarcoCiaramella <marcociaramella87@gmail.com>
2021-02-03 19:56:31 +0100

    minor changes

--- app/src/main/java/com/outofbound/rhinoengine/test/TestMesh.java
+++ app/src/main/java/com/outofbound/rhinoengine/test/TestMesh.java
@@ -10,11 +10,11 @@ public class TestMesh extends Mesh {
     public TestMesh(Context context){
         //super("test_mesh_2.ply");
         super("test_textured_mesh.ply", BitmapUtil.getBitmapFromAsset(context,"Material.001_Base_Color.png"));
-        position.y = -1.8f;
+        rotation.x = -90;
     }
 
     @Override
     public void doTransformation(float[] mMatrix, long ms) {
-        translate(mMatrix);
+        rotateX(mMatrix);
     }
 }
