5d704d31fd1bfaf9afc6d0d056d83a5eca5cbe17
MarcoCiaramella <marcociaramella87@gmail.com>
2021-02-04 19:01:52 +0100

    added rotation in BitmapUtil

--- rhinoenginelib/src/main/java/com/outofbound/rhinoenginelib/util/bitmap/BitmapUtil.java
+++ rhinoenginelib/src/main/java/com/outofbound/rhinoenginelib/util/bitmap/BitmapUtil.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 
 import java.io.IOException;
 
@@ -12,10 +13,16 @@ public class BitmapUtil {
     public static Bitmap getBitmapFromAsset(Context context, String fileName){
         AssetManager assetManager = context.getAssets();
         try {
-            return BitmapFactory.decodeStream(assetManager.open(fileName));
+            return rotate(BitmapFactory.decodeStream(assetManager.open(fileName)));
         } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    private static Bitmap rotate(Bitmap bitmap){
+        Matrix matrix = new Matrix();
+        matrix.postRotate(180);
+        return Bitmap.createBitmap(bitmap,0,0,bitmap.getWidth(),bitmap.getHeight(),matrix,true);
     }
 }
