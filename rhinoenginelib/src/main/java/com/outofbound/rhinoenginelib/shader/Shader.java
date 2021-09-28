8e4b688ffdef3dab0617c755d5bb81667d098d81
MarcoCiaramella <marcociaramella87@gmail.com>
2021-09-28 21:50:25 +0200

    added errors check

--- rhinoenginelib/src/main/java/com/outofbound/rhinoenginelib/shader/Shader.java
+++ rhinoenginelib/src/main/java/com/outofbound/rhinoenginelib/shader/Shader.java
@@ -1,5 +1,6 @@
 package com.outofbound.rhinoenginelib.shader;
 
+import android.content.Context;
 import android.opengl.GLES20;
 
 import com.outofbound.rhinoenginelib.engine.AbstractEngine;
@@ -12,20 +13,28 @@ public abstract class Shader {
 
     public Shader(String vs, String fs){
         program = GLES20.glCreateProgram();
-        GLES20.glAttachShader(program, compile(GLES20.GL_VERTEX_SHADER,
-                TextFile.read(AbstractEngine.getInstance().getContext(), vs)));
-        GLES20.glAttachShader(program, compile(GLES20.GL_FRAGMENT_SHADER,
-                TextFile.read(AbstractEngine.getInstance().getContext(), fs)));
+        GLES20.glAttachShader(program, compile(GLES20.GL_VERTEX_SHADER, AbstractEngine.getInstance().getContext(), vs));
+        GLES20.glAttachShader(program, compile(GLES20.GL_FRAGMENT_SHADER, AbstractEngine.getInstance().getContext(), fs));
         GLES20.glLinkProgram(program);
+        int[] params = new int[1];
+        GLES20.glGetProgramiv(program, GLES20.GL_LINK_STATUS, params, 0);
+        if (params[0] == GLES20.GL_FALSE){
+            throw new RuntimeException("Program linking failed:\n"+GLES20.glGetProgramInfoLog(program));
+        }
     }
 
     public abstract void bindData();
     public abstract void unbindData();
 
-    private int compile(int type, String source){
+    private int compile(int type, Context context, String filename){
         int shader = GLES20.glCreateShader(type);
-        GLES20.glShaderSource(shader, source);
+        GLES20.glShaderSource(shader, TextFile.read(context, filename));
         GLES20.glCompileShader(shader);
+        int[] params = new int[1];
+        GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, params, 0);
+        if (params[0] == GLES20.GL_FALSE){
+            throw new RuntimeException("Compilation of shader '" + filename + "' failed:\n"+GLES20.glGetShaderInfoLog(shader));
+        }
         return shader;
     }
 
